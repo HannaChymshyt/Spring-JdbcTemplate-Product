fc7af0df700f6bfc20018edb519df47577ffa196
Anna <anna.chymshyt@gmail.com>
2023-10-05 05:18:36 +0300

    Add Application.java

---
+++ src/main/java/org/example/app/Application.java
@@ -0,0 +1,11 @@
+package org.example.app;
+
+import org.example.app.utils.AppStarter;
+
+public class Application {
+
+    public static void main(String[] args) {
+        AppStarter.startApp();
+    }
+
+}
